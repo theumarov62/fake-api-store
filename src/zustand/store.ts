7d18fd8be59f8026bb4e331ce932e0607e0e1365
theumarov62 <maxbubaxonxoliqova62@gmail.com>
2026-03-05 20:14:02 +0500

    updated store.js

--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,22 +1,19 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, StateStorage } from "zustand/middleware";
 
 type ThemeStore = {
   darkMode: boolean;
   toggleTheme: () => void;
 };
 
-export const useThemeStore = create<ThemeStore>(
+export const useThemeStore = create<ThemeStore>()(
   persist(
     (set) => ({
       darkMode: false,
-      toggleTheme: () =>
-        set((state) => ({
-          darkMode: !state.darkMode,
-        })),
+      toggleTheme: () => set((state) => ({ darkMode: !state.darkMode })),
     }),
     {
-      name: "theme-storage", // localStorage key
+      name: "theme-storage",
     }
   )
 );
